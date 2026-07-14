9cfaee3d0fd46754989ff7f0504781bec50937c3
Nicolas Santos <nicolassantos14523@gmail.com>
2026-07-13 23:35:08 -0300

    fixa porta 8080

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def validar_cpf(cpf):
         return 'CPF INVÁLIDO'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=8080)
